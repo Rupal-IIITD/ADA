b2203cf0955eaa17e54b8f7f6f5ce84bcc1d9e61
Rupal-IIITD <rupal15081@iiitd.ac.in>
2017-02-27 10:36:29 +0530

    Update Inorder.c

--- lab1/Inorder.c
+++ lab1/Inorder.c
@@ -25,4 +25,38 @@ void printInorder(struct TreeNode* TreeNode)
         count++;
      printInorder(TreeNode->right);
 }
+ /* Utility function to create a new Binary Tree TreeNode */
+struct TreeNode* newTreeNode(int data)
+{
+  struct TreeNode *temp = new struct TreeNode;
+  temp->data = data;
+  temp->left = NULL;
+  temp->right = NULL;
  
+  return temp;
+}
+ 
+/* Driver function to test above functions */
+int main()
+{
+  struct TreeNode *root = NULL;
+  int x;
+ 
+  /* Constructing tree given in the above figure */
+  root = NULL;
+  // root->left = newTreeNode(-2);
+  // root->right = newTreeNode(6);
+  // root->left->left = newTreeNode(3);
+  // root->left->right = newTreeNode(2);
+  // root->right->left = NULL;
+  // root->right->right = newTreeNode(1);
+ 
+  answer(root);
+ 
+  // Print inorder traversal of the converted tree to test result of answer()
+  //printf("Inorder Traversal of the resultant tree is: \n");
+  printInorder(root);
+  printf("%d\n", count);
+ 
+  return 0;
+}
